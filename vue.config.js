488f1e3d3244b1c2a5268780194c060a50a3b87a
bigilisandro <bigilisandro@gmail.com>
2023-07-28 15:42:27 -0300

    remove router

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/memory-game-vue/'
-    : '/'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  publicPath: './',
+  outputDir:'dist',
+  assetsDir:'static'
+})
